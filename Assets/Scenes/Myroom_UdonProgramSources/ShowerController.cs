8f2c59e06518224ec56d994ff569397ebf4413eb
名前 <>
2024-12-04 15:07:02 +0900

    1204

---
+++ Assets/Scenes/Myroom_UdonProgramSources/ShowerController.cs
@@ -0,0 +1,38 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class ShowerController : UdonSharpBehaviour
+{
+    public ParticleSystem showerParticleSystem;
+    private bool isShowerOn = false;
+
+    void Start()
+    {
+        if (showerParticleSystem != null)
+        {
+            showerParticleSystem.Stop();
+        }
+    }
+
+    public override void Interact()
+    {
+        Debug.Log("Button Pressed"); // デバッグログ
+        if (showerParticleSystem != null)
+        {
+            if (isShowerOn)
+            {
+                showerParticleSystem.Stop();
+                Debug.Log("Shower Off"); // デバッグログ
+            }
+            else
+            {
+                showerParticleSystem.Play();
+                Debug.Log("Shower On"); // デバッグログ
+            }
+            
+            isShowerOn = !isShowerOn;
+        }
+    }
+}
---
+++ Assets/Scenes/VRCDefaultWorldScene_UdonProgramSources/Potato.cs
@@ -0,0 +1,27 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Potato : UdonSharpBehaviour
+{
+    public GameObject dish;
+    private AudioSource audioSource;
+  
+void Start()
+{
+    audioSource=GetComponent<AudioSource>();
+}
+
+
+public override void Interact()
+{
+    audioSource.Play();
+}
+void OnDrop() {
+    //   audioSource.Play();
+  gameObject.transform.position = dish.transform.position + new Vector3(0, 0.05f, 0);
+//   audioSource.Play();
+}
+}
